c301f37042445952a4f796600d7b013b06a90581
Jay Marzan <jaymblue5@gmail.com>
2024-04-30 15:21:16 -0600

    Eventually I want to make it web based

---
+++ src/search.py
@@ -0,0 +1,35 @@
+import os
+
+def searchFolder(user_input):
+    searched_files = []
+    for root, dirs, files in os.walk('src/data'):
+        for file in files:
+            if user_input in file.split()[0]:
+                searched_files.append(file)
+    return searched_files
+
+def printContents(selectedFile):
+    for root, dirs, files in os.walk('src/data'):
+        for file in files:
+            if selectedFile == file:
+                filepath = root + '/' + file
+                f = open(filepath, "r")
+                for line in f:
+                    print(line)
+
+if __name__ == "__main__":
+    user_input = input("Please input keyword: ")
+    # Test the functions
+    files = searchFolder(user_input)
+
+    print('---Selected files---')
+    fileDict = {}
+    count = 0
+    for i in files:
+        fileDict[count] = i
+        print(f"{count}: {i}")
+        count += 1
+
+    fileToDisplay = input("\nSelect file number: ")
+    print(f"\n---{fileDict[int(fileToDisplay)]} contents---")
+    printContents(fileDict[int(fileToDisplay)])
